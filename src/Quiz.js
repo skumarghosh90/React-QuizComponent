90ea7c04c8b016142f6ae3d24ddbcf44819fcd11
skumarghosh90 <gshark111@gmail.com>
2019-05-20 21:52:10 +0530

    init

---
+++ src/Quiz.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {quiz_position: 1};
+    }
+
+    render = () => (
+        <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+    );
+}
+
+export default Quiz;
