8f1f721ec4a98d763b51c41e295593017dfdcd61
skumarghosh90 <gshark111@gmail.com>
2019-05-20 21:56:07 +0530

    fix for old codebase

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,9 +9,9 @@ class Quiz extends React.Component {
         this.state = {quiz_position: 1};
     }
 
-    render = () => (
+    render = function() {
         <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
-    );
+    }
 }
 
 export default Quiz;
