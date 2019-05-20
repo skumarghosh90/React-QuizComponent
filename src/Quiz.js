c9dcadec57026d33d9471143ae942272e0189ed9
skumarghosh90 <gshark111@gmail.com>
2019-05-20 22:01:55 +0530

    minor changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,13 +4,16 @@ let quizData = require('./quiz_data.json');
 
 class Quiz extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {quiz_position: 1};
     }
 
-    render = function() {
-        <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+
+    render() {
+        return(
+            <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+        );
     }
 }
 
